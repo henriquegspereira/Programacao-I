16ed14cefd3f7b46daa38fbd93ffad35bf7de78e
henriquegspereira <henriquegpereira07@gmail.com>
2026-03-02 20:37:06 -0300

    First commit com Exemplo01 e README

---
+++ Exemplo01/Exemplo01/Controllers/OperatorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exemplo01.Controllers
+{
+    public class Operators : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
